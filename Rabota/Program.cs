d095ef7433a8150b516a32cc2daf3edadb238697
rustem <prorusteg@gmail.com>
2022-12-04 20:12:24 +0300

    r

---
+++ Rabota/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через запятую: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int Recalculation(int[] array)
+{
+    int caunt = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            caunt++;
+        }
+
+    }
+    return caunt;
+}
+PrintArray(arr);
+int result = Recalculation(arr);
+Console.WriteLine($"Результат: {result}");
---
+++ Rabota1/Program.cs
@@ -0,0 +1,35 @@
+﻿// // Задача 43: Напишите программу, 
+// которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double Xpoint(double bb1, double kk1, double bb2, double kk2)
+{
+    double xx = -(bb1 - bb2) / (kk1 - kk2);
+    xx = Math.Round(xx, 1);
+    return xx;
+}
+
+double Ypoint(double kk1, double bb1, double xx)
+{
+    double yy = kk1 * xx + bb1;
+    yy = Math.Round(yy, 1);
+    return yy;
+}
+
+double x = Xpoint(b1, k1, b2, k2);
+double y = Ypoint(k1, b1, x);
+x = Math.Round(x, 1);
+y = Math.Round(y, 1);
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
